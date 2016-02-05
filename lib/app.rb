96d45e7db7f82e73f9baec3de8e1f8219e3dea8d
Grzegorz Olszewski <grzegorz.olszewski10@gmail.com>
2016-02-05 15:48:01 +0100

    Added print method to get report in file, not in console. Ready for submission.

--- lib/app.rb
+++ lib/app.rb
@@ -1,53 +1,70 @@
 require 'json'
 
+def start
+setup_files
+create_report
+end
+def print (output)
+	$report_file.puts output
+end
 def setup_files
 	path = File.join(File.dirname(__FILE__), '../data/products.json')
 	file = File.read(path)
 	$products_hash = JSON.parse(file)
 	$report_file = File.new("report.txt", "w+")
 end
+def create_report
+print("Sales Report")
+print(Time.new)
+print(" ")
+print_all_about_products
+print_all_about_brands
+end
+
 ##############PRODUCTS METHODS#################
 
 def print_asci_art_product
-	puts "                     _            _       "
-	puts "                    | |          | |      "
-	puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
-	puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
-	puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
-	puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
-	puts "| |                                       "
-	puts "|_|                                       "
-	puts
+	print ("                     _            _       ")
+	print ("                    | |          | |      ")
+	print (" _ __  _ __ ___   __| |_   _  ___| |_ ___ ")
+	print ("| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|")
+	print ("| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\")
+	print ("| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/")
+	print ("| |                                       ")
+	print ("|_|                                       ")
+	print ( )
 end
 
 def print_all_about_products
+	print_asci_art_product
 	i = 0
 	while i < $products_hash["items"].length
 		print_name_of_toy(i)
+		print(" ")
 		print_full_price(i)
 		print_number_of_purchases(i)
 		print_amout_of_sales(i)
 		print_average_price_the_toy_sold_of(i)
 		print_average_discount(i)
-		puts
+		print (" ")
 		i = i + 1
 	end
 end
 
 def print_name_of_toy (position_in_array_of_toy)
-	puts $products_hash["items"][position_in_array_of_toy]["title"]
+	print ($products_hash["items"][position_in_array_of_toy]["title"])
 end
 
 def print_full_price (position_in_array_of_toy)
-	puts "Price of toy: $#{$products_hash["items"][position_in_array_of_toy]["full-price"]}"
+	print ("Price of toy: $#{$products_hash["items"][position_in_array_of_toy]["full-price"]}")
 end
 
 def print_number_of_purchases(position_in_array_of_toy)
-	puts "Number of purchases: #{$products_hash["items"][position_in_array_of_toy]["purchases"].size}"
+	print ("Number of purchases: #{$products_hash["items"][position_in_array_of_toy]["purchases"].size}")
 end
 
 def print_amout_of_sales (position_in_array_of_toy)
-	puts "Amount of sales: $#{calculate_amount_of_sales(position_in_array_of_toy)}"
+	print ("Amount of sales: $#{calculate_amount_of_sales(position_in_array_of_toy)}")
 end
 
 def calculate_amount_of_sales(position_in_array_of_toy)
@@ -61,7 +78,7 @@ def calculate_amount_of_sales(position_in_array_of_toy)
 end
 
 def print_average_price_the_toy_sold_of(position_in_array_of_toy)
-	puts "Average price: $#{calculate_average_price_the_toy_sold_of(position_in_array_of_toy)}" 
+	print ("Average price: $#{calculate_average_price_the_toy_sold_of(position_in_array_of_toy)}") 
 end
 
 def calculate_average_price_the_toy_sold_of(position_in_array_of_toy)
@@ -75,22 +92,23 @@ def calculate_average_discount(position_in_array_of_toy)
 end
 
 def print_average_discount(position_in_array_of_toy)
-	puts "Average discount: #{calculate_average_discount(position_in_array_of_toy).round(2)}%" 
+	print ("Average discount: #{calculate_average_discount(position_in_array_of_toy).round(2)}%")
 end
 
 
 ################BRANDS METHODS######################
 def print_asci_art_brand
-	puts " _                         _     "
-	puts "| |                       | |    "
-	puts "| |__  _ __ __ _ _ __   __| |___ "
-	puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
-	puts "| |_) | | | (_| | | | | (_| \\__ \\"
-	puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
-	puts
+	print (" _                         _     ")
+	print ("| |                       | |    ")
+	print ("| |__  _ __ __ _ _ __   __| |___ ")
+	print ("| '_ \\| '__/ _` | '_ \\ / _` / __|")
+	print ("| |_) | | | (_| | | | | (_| \\__ \\")
+	print ("|_.__/|_|  \\__,_|_| |_|\\__,_|___/")
+	print(" ")
 end
 
 def print_all_about_brands()
+	print_asci_art_brand
 	i = 0
 	uniq_brands = make_an_array_of_unique_brands
 	while i < uniq_brands.length do 
@@ -98,7 +116,8 @@ def print_all_about_brands()
 		number_of_toys = 0
 		sum_of_prices = 0
 		brand_revenue = 0
-		puts uniq_brands[i]
+		print (uniq_brands[i])
+		print(" ")
 		k = 0
 		while k < $products_hash["items"].length do
 			if uniq_brands[i] == $products_hash["items"][k]["brand"]
@@ -113,7 +132,7 @@ def print_all_about_brands()
 		print_stock(stock)
 		print_average_price_of_toy(average_price_of_toy)
 		print_brand_revenue(brand_revenue)
-		puts
+		print(" ")
 
 		i = i + 1
 	end
@@ -148,45 +167,17 @@ def calculate_average_price_of_toy(sum_of_prices,number_of_toys)
 	average_price_of_toy = (sum_of_prices/number_of_toys).round(2) 
 end
 def print_stock (stock)
-	puts "Stock: #{stock}"
+	print ("Stock: #{stock}")
 end
 
 def print_average_price_of_toy(average_price_of_toy)
-	puts "Average price of toy: $#{average_price_of_toy}"
+	print ("Average price of toy: $#{average_price_of_toy}")
 end
 
 def print_brand_revenue(brand_revenue)
-	puts "Brand's revenue: $#{brand_revenue}"
+	print ("Brand's revenue: $#{brand_revenue}")
 end
 
 
 #############################MAIN BODY#############################
-setup_files
-puts "Sales Report"
-# Print "Sales Report" in ascii art
-
-time = Time.new
-puts time
-puts
-# Print today's date
-
-print_asci_art_product
-# Print "Products" in ascii art
-
-print_all_about_products
-# For each product in the data set:
-	# Print the name of the toy
-	# Print the retail price of the toy
-	# Calculate and print the total number of purchases
-	# Calculate and print the total amount of sales
-	# Calculate and print the average price the toy sold for
-	# Calculate and print the average discount (% or $) based off the average sales price
-print_asci_art_brand
-# Print "Brands" in ascii art
-
-print_all_about_brands
-# For each brand in the data set:
-	# Print the name of the brand
-	# Count and print the number of the brand's toys we stock
-	# Calculate and print the average price of the brand's toys
-	# Calculate and print the total sales volume of all the brand's toys combined
+start
